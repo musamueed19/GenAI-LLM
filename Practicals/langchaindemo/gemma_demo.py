fb784d6cf92378900dd82b3f6a92c8c98259d6e8
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-08-25 21:13:44 +0500

    gemma 2b demo update

--- Practicals/langchaindemo/gemma_demo.py
+++ Practicals/langchaindemo/gemma_demo.py
@@ -1,10 +1,12 @@
 import os
 
-from langchain_community.chat_models import ChatOllama
+# from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
+
 
 llm = ChatOllama(model="gemma:2b")
 
-question = input("Hi! I am Dartrex. You can ask me any question?")
+question = input("Hi👋🏻! I am Dartrex. You can ask me any question?\n")
 
 response = llm.invoke(question)
 print(response.content)
